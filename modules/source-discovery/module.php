33503b7e85e3ae0fdd91e1264cf54c9d9922f029
Japur Ganteng <113220386+jayapurnama@users.noreply.github.com>
2026-09-10 22:20:26 +0700

    Load additive Unified Web Source UI

--- modules/source-discovery/module.php
+++ modules/source-discovery/module.php
@@ -2,7 +2,7 @@
 /**
  * Module: JaPur Source Discovery
  * Description: Isolated entrypoint for Unified Web Sumber discovery.
- * Module Version: 0.1.0
+ * Module Version: 0.1.1
  * Author: Japur Ganteng
  */
 if (!defined('ABSPATH')) exit;
@@ -10,4 +10,9 @@ if (!defined('ABSPATH')) exit;
 $loader = __DIR__ . '/web-source-bridge-loader.php';
 if (is_readable($loader)) {
     require_once $loader;
+}
+
+$ui = __DIR__ . '/web-source-ui.php';
+if (is_readable($ui)) {
+    require_once $ui;
 }
